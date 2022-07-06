0091153cd6ec2374d8f2e674a581d377a29c9e20
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-06 12:17:24 +0200

    add className icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <i class="fa-brands fa-twitter-square"></i>
-      <i class="fa-brands fa-facebook-square"></i>
-      <i class="fa-brands fa-instagram-square"></i>
-      <i class="fa-brands fa-github-square"></i>
+      <i className="fa-brands fa-twitter-square"></i>
+      <i className="fa-brands fa-facebook-square"></i>
+      <i className="fa-brands fa-instagram-square"></i>
+      <i className="fa-brands fa-github-square"></i>
     </div>
   )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -9,10 +9,10 @@ export default function Info() {
       <p className="title link-website">ricardonader.website</p>
       <div className="link-buttons">
         <a href="mailto:ricardoalexis.nader@gmail.com">
-          <button className="link-button"><i class="fa-solid fa-envelope"></i> Email</button>
+          <button className="link-button"><i className="fa-solid fa-envelope"></i> Email</button>
         </a>
         <a href="https://www.linkedin.com/in/ricardonader/">
-          <button className="link-button"><i class="fa-brands fa-linkedin"></i> Linkedin</button>
+          <button className="link-button"><i className="fa-brands fa-linkedin"></i> Linkedin</button>
         </a>
       </div>
     </div>
